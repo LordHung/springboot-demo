834f1e9459e0e9022510d5259cfae7ef7b5e42f2
Lord Hung <hunguit14@gmail.com>
2017-04-25 00:20:16 +0700

    add controller

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "Hello friend";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
