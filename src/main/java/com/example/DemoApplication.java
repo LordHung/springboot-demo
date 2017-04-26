f52aaff7576013293114e46250a77a2eab4801b2
Lord Hung <hunguit14@gmail.com>
2017-04-27 00:00:01 +0700

    update

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,16 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+
 @SpringBootApplication
-@Controller
 public class DemoApplication {
-    @RequestMapping("/")
-    @ResponseBody
-    public String home(){
-        return "Hello friend";
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
