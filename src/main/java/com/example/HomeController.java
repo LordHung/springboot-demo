c7b9f15702294732aa06fcac5f104474dd3c9b00
Lord Hung <hunguit14@gmail.com>
2017-04-27 00:01:23 +0700

    update

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -2,6 +2,7 @@ import com.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
